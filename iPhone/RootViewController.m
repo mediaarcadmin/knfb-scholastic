23444ad1beddf87e38c700eb3ef464b64b8dfeb1
John S. Eddie <john.eddie@bitwink.com>
2011-02-08 14:46:12 +0000

    Deleted RootViewController as it's been renamed

---
+++ iPhone/RootViewController.m
@@ -0,0 +1,433 @@
+//
+//  RootViewController.m
+//  Tester
+//
+//  Created by John S. Eddie on 30/12/2010.
+//  Copyright 2010 BitWink Limited. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+#import "SCHSettingsViewController.h"
+#import "SCHBookShelfViewController.h"
+#import "SCHWebServiceSync.h"
+#import "SCHLoginViewController.h"
+#import "SCHAuthenticationManager.h"
+#import "SCHLibreAccessWebService.h"
+#import "SCHContentProfileItem+Extensions.h"
+#import "SCHProfileItem+Extensions.h"
+#import "SCHContentMetadataItem+Extensions.h"
+#import "SCHMultipleBookshelvesController.h"
+
+// Cell Icons 
+static NSString * const kRootViewControllerProfileIcon = @"Profile.png";
+static NSString * const kRootViewControllerProfileLockedIcon = @"ProfileLocked.png";
+static NSString * const kRootViewControllerSettingsIcon = @"Settings.png";
+
+// Static cells
+static NSInteger const kRootViewControllerSettingsRow = 1;
+
+@interface RootViewController ()
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath;
+@end
+
+
+@implementation RootViewController
+
+@synthesize headerView;
+@synthesize settingsController;
+@synthesize loginController;
+@synthesize webServiceSync;
+@synthesize fetchedResultsController=fetchedResultsController_, managedObjectContext=managedObjectContext_;
+
+
+#pragma mark -
+#pragma mark View lifecycle
+
+- (void)awakeFromNib
+{
+	[super awakeFromNib];
+	
+	self.webServiceSync = [[SCHWebServiceSync alloc] init];
+	self.webServiceSync.managedObjectContext = [self.fetchedResultsController managedObjectContext];	
+	[self.webServiceSync release];	
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Set up the edit and add buttons.
+    UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(refreshTable)];
+    self.navigationItem.rightBarButtonItem = refreshButton;
+    [refreshButton release], refreshButton = nil;
+	
+	// Setup the table view header
+	self.tableView.tableHeaderView = self.headerView;
+}
+
+- (void)viewDidUnload {
+    // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
+    // For example: self.myOutlet = nil;
+	
+	self.headerView = nil;
+	self.settingsController = nil;
+}
+
+
+// Implement viewWillAppear: to do additional setup before the view is presented.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+#ifndef LOCALDEBUG	
+	SCHAuthenticationManager *authenticationManager = [SCHAuthenticationManager sharedAuthenticationManager];
+	
+	if ([authenticationManager hasUsernameAndPassword] == NO) {
+		[self presentModalViewController:self.loginController animated:NO];	
+		[self.loginController removeCancelButton];
+	}
+#endif
+}
+
+/*
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+*/
+/*
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+*/
+
+/*
+ // Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations.
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+ */
+
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
+    id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:indexPath.section];
+	NSInteger managedObjectEnd = [sectionInfo numberOfObjects] - 1;
+	
+	if (indexPath.row == (managedObjectEnd + kRootViewControllerSettingsRow)) {
+		cell.textLabel.text = NSLocalizedString(@"Settings & Parental Controls", @"");
+		cell.imageView.image = [UIImage imageNamed:kRootViewControllerSettingsIcon];		
+	} else {
+		NSManagedObject *managedObject = [self.fetchedResultsController objectAtIndexPath:indexPath];
+		cell.textLabel.text = [NSString stringWithFormat:@"%@%@", 
+							   [[managedObject valueForKey:kSCHLibreAccessWebServiceScreenName] description], 
+							   NSLocalizedString(@"'s Books", @"")];
+		if ([[managedObject valueForKey:kSCHLibreAccessWebServiceProfilePasswordRequired] boolValue] == NO) {
+			cell.imageView.image = [UIImage imageNamed:kRootViewControllerProfileIcon];
+		} else {
+			cell.imageView.image = [UIImage imageNamed:kRootViewControllerProfileLockedIcon];
+		}
+	}
+}
+
+
+#pragma mark -
+#pragma mark Refresh the table
+
+- (void)refreshTable {
+    
+//    // Create a new instance of the entity managed by the fetched results controller.
+//    NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
+//    NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
+//    NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
+//    
+//    // If appropriate, configure the new managed object.
+//    [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
+//    
+//    // Save the context.
+//    NSError *error = nil;
+//    if (![context save:&error]) {
+//        /*
+//         Replace this implementation with code to handle the error appropriately.
+//         
+//         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+//         */
+//        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+//        abort();
+//    }
+	
+	if ([self.webServiceSync update] == NO ) {
+		UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Give me a moment"
+															 message:@"I am just requesting access. Normal service will resume shortly."
+															delegate:nil 
+												   cancelButtonTitle:NSLocalizedString(@"OK", @"OK")
+												   otherButtonTitles:nil]; 
+		[errorAlert show]; 
+		[errorAlert release];		
+	}
+}
+
+
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+
+    // Prevent new objects being added when in editing mode.
+    [super setEditing:(BOOL)editing animated:(BOOL)animated];
+    self.navigationItem.rightBarButtonItem.enabled = !editing;
+}
+
+
+#pragma mark -
+#pragma mark Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return [[self.fetchedResultsController sections] count];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:section];
+    return [sectionInfo numberOfObjects] + kRootViewControllerSettingsRow;
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+		cell.textLabel.font = [UIFont systemFontOfSize:14];
+    }
+    
+    // Configure the cell.
+    [self configureCell:cell atIndexPath:indexPath];
+    
+    return cell;
+}
+
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the managed object for the given index path
+        NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
+        [context deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
+        
+        // Save the context.
+        NSError *error = nil;
+        if (![context save:&error]) {
+            /*
+             Replace this implementation with code to handle the error appropriately.
+             
+             abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+             */
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+    }   
+}
+
+
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // The table view should not be re-orderable.
+    return NO;
+}
+
+- (void)pushBookshelvesControllerWithBooks:(NSArray *)books
+ {
+	SCHMultipleBookshelvesController *bookshelvesController = nil;
+	if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+		bookshelvesController = [[SCHMultipleBookshelvesController alloc] initWithNibName:NSStringFromClass([SCHMultipleBookshelvesController class]) bundle:nil managedObjectContext:self.managedObjectContext books:books];
+	}
+		
+	[self.navigationController pushViewController:bookshelvesController animated:YES];
+
+}
+
+#pragma mark -
+#pragma mark Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Navigation logic may go here -- for example, create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     NSManagedObject *selectedObject = [[self fetchedResultsController] objectAtIndexPath:indexPath];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+	
+    id <NSFetchedResultsSectionInfo> sectionInfo = [[self.fetchedResultsController sections] objectAtIndex:indexPath.section];
+	NSInteger managedObjectEnd = [sectionInfo numberOfObjects] - 1;
+	
+	if (indexPath.row == (managedObjectEnd + kRootViewControllerSettingsRow)) {
+		[self.navigationController pushViewController:self.settingsController animated:YES];
+	} else {
+		// controller to view book shelf with books filtered to profile		
+		SCHProfileItem *selectedObject = [[self fetchedResultsController] objectAtIndexPath:indexPath];
+				
+		[self pushBookshelvesControllerWithBooks:[selectedObject allContentMetadataItems]];
+	}	
+}
+
+
+#pragma mark -
+#pragma mark Fetched results controller
+
+- (NSFetchedResultsController *)fetchedResultsController {
+    
+    if (fetchedResultsController_ != nil) {
+        return fetchedResultsController_;
+    }
+    
+    /*
+     Set up the fetched results controller.
+    */
+    // Create the fetch request for the entity.
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    // Edit the entity name as appropriate.
+    NSEntityDescription *entity = [NSEntityDescription entityForName:kSCHProfileItem inManagedObjectContext:self.managedObjectContext];
+    [fetchRequest setEntity:entity];
+    
+    // Set the batch size to a suitable number.
+    [fetchRequest setFetchBatchSize:20];
+    
+    // Edit the sort key as appropriate.
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:kSCHLibreAccessWebServiceScreenName ascending:NO];
+    NSArray *sortDescriptors = [[NSArray alloc] initWithObjects:sortDescriptor, nil];
+    
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    
+    // Edit the section name key path and cache name if appropriate.
+    // nil for section name key path means "no sections".
+    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:@"Root"];
+    aFetchedResultsController.delegate = self;
+    self.fetchedResultsController = aFetchedResultsController;
+    
+    [aFetchedResultsController release];
+    [fetchRequest release];
+    [sortDescriptor release];
+    [sortDescriptors release];
+    
+    NSError *error = nil;
+    if (![fetchedResultsController_ performFetch:&error]) {
+        /*
+         Replace this implementation with code to handle the error appropriately.
+         
+         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+         */
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    return fetchedResultsController_;
+}    
+
+
+#pragma mark -
+#pragma mark Fetched results controller delegate
+
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView beginUpdates];
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+    
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath {
+    
+    UITableView *tableView = self.tableView;
+    
+    switch(type) {
+            
+        case NSFetchedResultsChangeInsert:
+            [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+            [self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
+            break;
+            
+        case NSFetchedResultsChangeMove:
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath]withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView endUpdates];
+}
+
+
+/*
+// Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed. 
+ 
+ - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    // In the simplest, most efficient, case, reload the table view.
+    [self.tableView reloadData];
+}
+ */
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Relinquish ownership any cached data, images, etc that aren't in use.
+}
+
+- (void)dealloc {
+	self.headerView = nil;
+	self.settingsController = nil;
+	self.webServiceSync = nil;
+	
+    [fetchedResultsController_ release];
+    [managedObjectContext_ release];
+    [super dealloc];
+}
+
+
+@end
+
