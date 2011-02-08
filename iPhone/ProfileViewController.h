ea123bcde834153f8ec12df80a5f11399fb61488
John S. Eddie <john.eddie@bitwink.com>
2011-02-08 14:59:32 +0000

    This time add the ProfileViewController files

---
+++ iPhone/ProfileViewController.h
@@ -0,0 +1,33 @@
+//
+//  RootViewController.h
+//  Tester
+//
+//  Created by John S. Eddie on 30/12/2010.
+//  Copyright 2010 BitWink Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+
+@class SCHSettingsViewController;
+@class SCHWebServiceSync;
+@class SCHLoginViewController;
+
+@interface ProfileViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+@private
+    NSFetchedResultsController *fetchedResultsController_;
+    NSManagedObjectContext *managedObjectContext_;
+}
+
+@property (nonatomic, retain) IBOutlet UIView *headerView;
+@property (nonatomic, retain) IBOutlet SCHSettingsViewController *settingsController;
+@property (nonatomic, retain) IBOutlet SCHLoginViewController *loginController;
+
+@property (nonatomic, retain) SCHWebServiceSync *webServiceSync;
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+
+@end
