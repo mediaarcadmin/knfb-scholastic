bf3d5bfb786fee5f28494da1a48636bb3f44858d
John S. Eddie <john.eddie@bitwink.com>
2011-03-08 16:36:34 +0000

    User Default strings file

---
+++ Shared/SCHUserDefaults.h
@@ -0,0 +1,10 @@
+/*
+ *  SCHUserDefaults.h
+ *  Scholastic
+ *
+ *  Created by John S. Eddie on 08/03/2011.
+ *  Copyright 2011 BitWink. All rights reserved.
+ *
+ */
+
+static NSString * const kSCHUserDefaultsPerformedFirstSyncUpToBooks = @"PerformedFirstSyncUpToBooks";
