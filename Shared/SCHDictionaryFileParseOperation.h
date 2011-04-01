541b0dd841f288e8bd3509f1bf957f2c0b18e099
John S. Eddie <john.eddie@bitwink.com>
2011-04-01 13:45:19 +0100

    Changed setting the raw password to use SHA1 instead of MD5

---
+++ Shared/SCHDictionaryFileParseOperation.h
@@ -0,0 +1,16 @@
+//
+//  SCHDictionaryFileParseOperation.h
+//  Scholastic
+//
+//  Created by John S. Eddie on 29/03/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SCHDictionaryFileParseOperation : NSOperation {
+    
+}
+
+@end
