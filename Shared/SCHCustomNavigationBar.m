025c87c40adf5cf82f5f10de16c021ab6afde8c5
Matt Farrugia <matt.farrugia@bitwink.com>
2011-04-19 09:41:45 +0100

    Simplified customnav bar

--- Shared/SCHCustomNavigationBar.m
+++ Shared/SCHCustomNavigationBar.m
@@ -11,15 +11,19 @@
 @interface SCHCustomNavigationBar ()
 
 @property (nonatomic, retain) UIImageView *backgroundView;
-@property (nonatomic, retain) UIColor *originalBackgroundColor;
 
 @end 
 
 @implementation SCHCustomNavigationBar
 
-@dynamic backgroundImage;
+@synthesize backgroundImage;
 @synthesize backgroundView;
-@synthesize originalBackgroundColor;
+
+- (void)dealloc
+{
+    [backgroundView release], backgroundView = nil;
+    [super dealloc];
+}
 
 - (void) layoutSubviews
 {
@@ -29,36 +33,14 @@
 
 - (void)setBackgroundImage:(UIImage*)image
 {
-    if (self.originalBackgroundColor == nil) {
-        self.originalBackgroundColor = self.backgroundColor;
-    }
-
-    if (image == nil) {
-        [backgroundView removeFromSuperview];
-        [backgroundView release], backgroundView = nil;
-        self.backgroundColor = self.backgroundColor;                
-        [self setNeedsDisplay];            
-    } else if (image != self.backgroundView.image) {
-        backgroundView = [[UIImageView alloc] initWithFrame:self.frame];
-        self.backgroundView.image = image;
-        
-        self.clipsToBounds = NO;
-        self.backgroundColor = [UIColor clearColor];   
-        
-        [self insertSubview:self.backgroundView atIndex:0];
-        
-        CGRect rect = self.frame;
-        NSLog(@"Rect: %@", NSStringFromCGRect(rect));
-        rect.size.height = self.backgroundView.image.size.height;
-        rect.origin.y = -1;
-        self.backgroundView.frame = rect;
-        NSLog(@"Rect: %@", NSStringFromCGRect(rect));
-        NSLog(@"Super rect: %@", NSStringFromCGRect(self.superview.frame));
-        
-        [self.backgroundView setAutoresizingMask:UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth];
-        
-        [self setNeedsDisplay];            
-    }
+    
+    CGRect imageFrame = self.bounds;
+    imageFrame.size.height = image.size.height;
+    imageFrame.origin.y = -1;
+    [self.backgroundView setImage:image];
+    [self.backgroundView setFrame:imageFrame];
+    
+    [self setNeedsLayout];
 }
 
 - (UIImage *)backgroundImage
@@ -66,5 +48,15 @@
     return(backgroundView.image);
 }
 
+- (UIImageView *)backgroundView
+{
+    if (!backgroundView) {
+        backgroundView = [[UIImageView alloc] initWithFrame:CGRectZero];
+        self.clipsToBounds = NO;
+        [self addSubview:backgroundView];
+    }
+    
+    return backgroundView;
+}
 
 @end
