ee98e9b8f563eb8ef20427c7249b65ad66ce9d67
John S. Eddie <john.eddie@bitwink.com>
2011-04-28 09:07:38 +0100

    SCHContentProfileItem no longer inherits from SCHFavorite.

---
+++ Shared/UIColor+Extensions.h
@@ -0,0 +1,14 @@
+//
+//  Category.h
+//  Scholastic
+//
+//  Created by John S. Eddie on 27/04/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIColor (Category)
+
+@end
