fc8ed03e071a730ae5fe38211c2cfc546cbf52c9
John S. Eddie <john.eddie@bitwink.com>
2011-05-06 12:16:39 +0100

    Annotations now handle inserting and deleted annotations

--- Shared/SCHBookAnnotations.m
+++ Shared/SCHBookAnnotations.m
@@ -10,6 +10,7 @@
 
 #import "SCHPrivateAnnotations.h"
 #import "SCHLibreAccessWebService.h"
+#import "SCHAnnotationSyncComponent.h"
 
 @interface SCHBookAnnotations ()
 
@@ -34,18 +35,28 @@
     self = [super init];
     if (self) {
         privateAnnotations = [aPrivateAnnotation retain];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshData) name:kSCHAnnotationSyncComponentComplete object:nil];        
     }
     return(self);
 }
 
 - (void)dealloc 
 {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
     [privateAnnotations release], privateAnnotations = nil;
     [sortedBookmarks release], sortedBookmarks = nil;
     [sortedHighlights release], sortedHighlights = nil;
     [sortedNotes release], sortedNotes = nil;
     
     [super dealloc];
+}
+
+- (void)refreshData
+{
+    self.sortedBookmarks = nil;
+    self.sortedHighlights = nil;
+    self.sortedNotes = nil;
 }
 
 #pragma mark - Accessor methods
@@ -56,9 +67,18 @@
         NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:kSCHLibreAccessWebServicePage ascending:YES];
         self.sortedBookmarks = [self.privateAnnotations.Bookmarks sortedArrayUsingDescriptors:
                            [NSArray arrayWithObject:sortDescriptor]];
+        
+        // remove deleted objects
+        self.sortedBookmarks = [self.sortedHighlights filteredArrayUsingPredicate:
+                                 [NSPredicate predicateWithFormat:@"State != %@", [NSNumber numberWithStatus:kSCHStatusDeleted]]];                
     }
 
     return(self.sortedBookmarks);
+}
+
+- (void)addBookmark:(SCHBookmark *)newBookmark
+{
+    [self.privateAnnotations addBookmarksObject:newBookmark];
 }
 
 - (NSArray *)highlightsForPage:(NSUInteger)page
@@ -72,10 +92,14 @@
         NSSortDescriptor *sortDescriptor2 = [NSSortDescriptor sortDescriptorWithKey:kSCHLibreAccessWebServiceStart ascending:YES];
         self.sortedHighlights = [self.privateAnnotations.Highlights sortedArrayUsingDescriptors:
                                  [NSArray arrayWithObjects:sortDescriptor1, sortDescriptor2, nil]];
+        
+        // remove deleted objects
+        self.sortedHighlights = [self.sortedHighlights filteredArrayUsingPredicate:
+                                 [NSPredicate predicateWithFormat:@"State != %@", [NSNumber numberWithStatus:kSCHStatusDeleted]]];        
     }
 
     // search for page
-    [self.sortedHighlights enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop){
+    [self.sortedHighlights enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
         if ([[obj page] integerValue] == page) {
             if (found == NO) {
                 pageRange.location = idx;
@@ -95,15 +119,29 @@
     return(ret);
 }
 
+- (void)addHighlight:(SCHHighlight *)newHighlight
+{
+    [self.privateAnnotations addHighlightsObject:newHighlight];
+}
+
 - (NSArray *)notes
 {
     if (self.sortedNotes == nil) {
         NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:kSCHLibreAccessWebServicePage ascending:YES];
         self.sortedNotes = [self.privateAnnotations.Notes sortedArrayUsingDescriptors:
                           [NSArray arrayWithObject:sortDescriptor]];
+        
+        // remove deleted objects
+        self.sortedNotes = [self.sortedHighlights filteredArrayUsingPredicate:
+                                 [NSPredicate predicateWithFormat:@"State != %@", [NSNumber numberWithStatus:kSCHStatusDeleted]]];        
     }
     
     return(self.sortedNotes);
+}
+
+- (void)addNote:(SCHNote *)newNote
+{
+    [self.privateAnnotations addNotesObject:newNote];
 }
 
 - (SCHFavorite *)favorite
