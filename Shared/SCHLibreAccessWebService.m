6439c4099c632c853ef3697de8fb336d705dcf46
John S. Eddie <john.eddie@bitwink.com>
2011-06-03 00:23:31 +0100

    Added some protection for nil color properties coming from the server

--- Shared/SCHLibreAccessWebService.m
+++ Shared/SCHLibreAccessWebService.m
@@ -933,7 +933,7 @@ static NSInteger const kSCHLibreAccessWebServiceVaid = 33;
 		
 		[objects setObject:[self objectFromTranslate:anObject.id_] forKey:kSCHLibreAccessWebServiceID];
 		[objects setObject:[NSNumber numberWithSaveAction:anObject.action] forKey:kSCHLibreAccessWebServiceAction];
-		[objects setObject:[UIColor BITcolorWithHexString:anObject.color] forKey:kSCHLibreAccessWebServiceColor];
+		[objects setObject:[self objectFromTranslate:[UIColor BITcolorWithHexString:anObject.color]] forKey:kSCHLibreAccessWebServiceColor];
 		[objects setObject:[self objectFromLocationText:anObject.location] forKey:kSCHLibreAccessWebServiceLocation];
 		[objects setObject:[self objectFromTranslate:anObject.endPage] forKey:kSCHLibreAccessWebServiceEndPage];
 		[objects setObject:[self objectFromTranslate:anObject.version] forKey:kSCHLibreAccessWebServiceVersion];		
@@ -987,7 +987,7 @@ static NSInteger const kSCHLibreAccessWebServiceVaid = 33;
 		[objects setObject:[self objectFromTranslate:anObject.id_] forKey:kSCHLibreAccessWebServiceID];
 		[objects setObject:[NSNumber numberWithSaveAction:anObject.action] forKey:kSCHLibreAccessWebServiceAction];
 		[objects setObject:[self objectFromLocationGraphics:anObject.location] forKey:kSCHLibreAccessWebServiceLocation];
-		[objects setObject:[UIColor BITcolorWithHexString:anObject.color] forKey:kSCHLibreAccessWebServiceColor];
+		[objects setObject:[self objectFromTranslate:[UIColor BITcolorWithHexString:anObject.color]] forKey:kSCHLibreAccessWebServiceColor];
 		[objects setObject:[self objectFromTranslate:anObject.value] forKey:kSCHLibreAccessWebServiceValue];
 		[objects setObject:[self objectFromTranslate:anObject.version] forKey:kSCHLibreAccessWebServiceVersion];
 		[objects setObject:[self objectFromTranslate:anObject.lastmodified] forKey:kSCHLibreAccessWebServiceLastModified];
