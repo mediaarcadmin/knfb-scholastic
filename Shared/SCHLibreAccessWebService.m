59760ca32f778885296a8acdb57f5dc42c1dffb8
John S. Eddie <john.eddie@bitwink.com>
2011-06-03 12:27:33 +0100

    Only sync annotation objects that need to be sync'd

--- Shared/SCHLibreAccessWebService.m
+++ Shared/SCHLibreAccessWebService.m
@@ -1348,33 +1348,40 @@ static NSInteger const kSCHLibreAccessWebServiceVaid = 33;
 	}
 }												
 
+// only creates annotation objects that have a status, i.e. need to be saved
 - (void)fromObject:(NSDictionary *)object intoPrivateAnnotations:(LibreAccessServiceSvc_PrivateAnnotations *)intoObject
 {
 	if (object != nil && intoObject != nil) {
 		intoObject.Highlights = [[LibreAccessServiceSvc_Highlights alloc] init];
 		for (NSDictionary *item in [self fromObjectTranslate:[object valueForKey:kSCHLibreAccessWebServiceHighlights]]) {
-			LibreAccessServiceSvc_Highlight *highlight = [[LibreAccessServiceSvc_Highlight alloc] init];
-			[self fromObject:item intoHighlight:highlight];
-			[intoObject.Highlights addHighlight:highlight];
-			[highlight release], highlight = nil;
+			if ([[self fromObjectTranslate:[item valueForKey:kSCHLibreAccessWebServiceAction]] saveActionValue] != kSCHSaveActionsNone) {
+                LibreAccessServiceSvc_Highlight *highlight = [[LibreAccessServiceSvc_Highlight alloc] init];
+                [self fromObject:item intoHighlight:highlight];
+                [intoObject.Highlights addHighlight:highlight];
+                [highlight release], highlight = nil;
+            }
 		}
 		[intoObject.Highlights release];
 		
 		intoObject.Notes = [[LibreAccessServiceSvc_Notes alloc] init];
 		for (NSDictionary *item in [self fromObjectTranslate:[object valueForKey:kSCHLibreAccessWebServiceNotes]]) {
-			LibreAccessServiceSvc_Note *note = [[LibreAccessServiceSvc_Note alloc] init];
-			[self fromObject:item intoNote:note];
-			[intoObject.Notes addNote:note];
-			[note release], note = nil;
+            if ([[self fromObjectTranslate:[item valueForKey:kSCHLibreAccessWebServiceAction]] saveActionValue] != kSCHSaveActionsNone) {
+                LibreAccessServiceSvc_Note *note = [[LibreAccessServiceSvc_Note alloc] init];
+                [self fromObject:item intoNote:note];
+                [intoObject.Notes addNote:note];
+                [note release], note = nil;
+            }
 		}									
 		[intoObject.Notes release];
 		
 		intoObject.Bookmarks = [[LibreAccessServiceSvc_Bookmarks alloc] init];
 		for (NSDictionary *item in [self fromObjectTranslate:[object valueForKey:kSCHLibreAccessWebServiceBookmarks]]) {
-			LibreAccessServiceSvc_Bookmark *bookmark = [[LibreAccessServiceSvc_Bookmark alloc] init];
-			[self fromObject:item intoBookmark:bookmark];
-			[intoObject.Bookmarks addBookmark:bookmark];
-			[bookmark release], bookmark = nil;
+            if ([[self fromObjectTranslate:[item valueForKey:kSCHLibreAccessWebServiceAction]] saveActionValue] != kSCHSaveActionsNone) {
+                LibreAccessServiceSvc_Bookmark *bookmark = [[LibreAccessServiceSvc_Bookmark alloc] init];
+                [self fromObject:item intoBookmark:bookmark];
+                [intoObject.Bookmarks addBookmark:bookmark];
+                [bookmark release], bookmark = nil;
+            }
 		}									
 		[intoObject.Bookmarks release];
 		
