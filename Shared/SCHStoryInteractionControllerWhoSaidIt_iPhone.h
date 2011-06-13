6711c1a31e34f9680b57549b05ef71748501abaf
Neil Gall <neil.gall@bitwink.com>
2011-06-13 16:15:05 +0100

    Add support for iPad/iPhone-specific Story Interaction controllers. iPhone layout fixes.

---
+++ Shared/SCHStoryInteractionControllerWhoSaidIt_iPhone.h
@@ -0,0 +1,16 @@
+//
+//  SCHStoryInteractionControllerWhoSaidIt_iPhone.h
+//  Scholastic
+//
+//  Created by Neil Gall on 13/06/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SCHStoryInteractionControllerWhoSaidIt_iPhone : SCHStoryInteractionController {
+    
+}
+
+@end
---
+++ Shared/SCHStoryInteractionControllerWhoSaidIt_iPhone.m
@@ -0,0 +1,14 @@
+//
+//  SCHStoryInteractionControllerWhoSaidIt_iPhone.m
+//  Scholastic
+//
+//  Created by Neil Gall on 13/06/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import "SCHStoryInteractionControllerWhoSaidIt_iPhone.h"
+
+
+@implementation SCHStoryInteractionControllerWhoSaidIt_iPhone
+
+@end
