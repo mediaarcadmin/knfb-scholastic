79835c8788f65b2eb572b965215b7ff6d1d42421
John S. Eddie <john.eddie@bitwink.com>
2011-06-15 13:13:38 +0100

    Update audio for starting letter story interaction

--- Shared/SCHStoryInteractionStartingLetter.h
+++ Shared/SCHStoryInteractionStartingLetter.h
@@ -32,7 +32,22 @@
 // array of SCHStoryInteractionStartingLetterQuestion
 @property (nonatomic, retain) NSArray *questions;
 
+// XPSProvider-relative path for intro audio
+- (NSString *)introductionAudioPath;
+
 // XPSProvider-relative path for question audio
 - (NSString *)audioPathForQuestion;
+
+// XPS-relative path for 'starts with'
+- (NSString *)audioPathForStartsWith;
+
+// XPS-relative path for the letter
+- (NSString *)audioPathForLetter;
+
+// XPS-relative path for 'doesnt start with'
+- (NSString *)audioPathForDoesntStartWith;
+
+// XPS-relative path for 'you found them all'
+- (NSString *)audioPathForYouFoundThemAll;
 
 @end
