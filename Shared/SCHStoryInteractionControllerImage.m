8c23f7c1ebf1819950e693066a33752abaa40ce9
John S. Eddie <john.eddie@bitwink.com>
2011-06-20 14:55:08 +0100

    Ticket #237 added zoom and scroll to iPhone picture story interaction

--- Shared/SCHStoryInteractionControllerImage.m
+++ Shared/SCHStoryInteractionControllerImage.m
@@ -13,10 +13,12 @@
 
 @implementation SCHStoryInteractionControllerImage
 
+@synthesize scrollView;
 @synthesize imageView;
 
 - (void)dealloc
 {
+    [scrollView release], scrollView = nil;
     [imageView release], imageView = nil;
     
     [super dealloc];
@@ -27,6 +29,43 @@
     NSString *imagePath = [(SCHStoryInteractionImage *)self.storyInteraction imagePath];
     NSData *imageData = [self.xpsProvider dataForComponentAtPath:imagePath];
     self.imageView.image = [UIImage imageWithData:imageData];
+
+    if ((UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) == YES) {
+        [self.scrollView addSubview:self.imageView];
+        self.scrollView.delegate = self;
+        
+        CGSize imageSize = self.imageView.image.size;
+        if (imageSize.width > 0.0 && imageSize.height > 0.0) {
+            CGRect rect = self.imageView.bounds;
+            rect.size = imageSize;
+            self.imageView.bounds = rect;        
+            self.imageView.frame = rect;
+            self.scrollView.contentSize = imageSize;
+            
+            CGFloat widthScale = CGRectGetWidth(self.scrollView.bounds) / imageSize.width;
+            CGFloat heightScale = CGRectGetHeight(self.scrollView.bounds) / imageSize.height;
+            self.scrollView.maximumZoomScale = MAX(widthScale, heightScale) * 1.5;    // restrict the zoom so we don't completely pixelize the image 
+            self.scrollView.minimumZoomScale = MIN(widthScale, heightScale);
+            
+            self.scrollView.zoomScale = widthScale;
+        }
+    } else {
+        self.scrollView.hidden = YES;
+    } 
+}
+
+#pragma mark - UIScrollView delegate methods
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return(self.imageView);
+}
+
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView 
+                       withView:(UIView *)view 
+                        atScale:(float)scale
+{
+        // nop - method included to implement zooming
 }
 
 @end
