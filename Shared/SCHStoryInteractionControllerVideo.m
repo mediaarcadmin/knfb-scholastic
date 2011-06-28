7c8e53c3e584ed44772c6b2604e63a2b58006d8d
John S. Eddie <john.eddie@bitwink.com>
2011-06-28 16:03:59 +0100

    [completed 334] Narrator now stops when playing the video and video pauses when playing the movie

--- Shared/SCHStoryInteractionControllerVideo.m
+++ Shared/SCHStoryInteractionControllerVideo.m
@@ -104,15 +104,10 @@ static CGFloat const kSCHStoryInteractionControllerVideoBorderWidth = 4.0;
 
 - (IBAction)playAudioButtonTapped:(id)sender
 {
-    BOOL iWasPlaying = self.moviePlayer.playbackState == MPMoviePlaybackStatePlaying;
     NSString *path = [self.storyInteraction audioPathForQuestion];
     if (path != nil) {
         [self pause];
-        [self playAudioAtPath:path completion:^{
-            if (iWasPlaying == YES) {
-                [self play];
-            }
-        }];
+        [self playAudioAtPath:path completion:nil];
     }   
 }
 
@@ -123,6 +118,7 @@ static CGFloat const kSCHStoryInteractionControllerVideoBorderWidth = 4.0;
 
 - (void)play
 {
+    [self cancelQueuedAudioExecutingSynchronizedBlocksImmediately];
     [self.moviePlayer play];
 }
 
