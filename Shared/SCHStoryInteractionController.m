222fdf791154495c57057314e83df15c9aed7ba8
Neil Gall <neil.gall@bitwink.com>
2011-07-06 10:36:47 +0100

    [completed: 423] lock Touch the Page in landscape orientation once it gets there the first time; simulate a transition back to portrait on close if the device is in portrait orientation.

--- Shared/SCHStoryInteractionController.m
+++ Shared/SCHStoryInteractionController.m
@@ -273,38 +273,6 @@
     [self presentInHostView:host withInterfaceOrientation:self.interfaceOrientation];
 }
 
-- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
-{
-    if (!self.containerView.superview) {
-        return;
-    }
-    
-    CGFloat superviewWidth = CGRectGetWidth(self.containerView.superview.bounds);
-    CGFloat superviewHeight = CGRectGetHeight(self.containerView.superview.bounds);
-    CGRect superviewBounds = CGRectMake(0, 0, MAX(superviewWidth, superviewHeight), MIN(superviewWidth, superviewHeight));
-    CGPoint superviewCenter = CGPointMake(floorf(CGRectGetMidX(superviewBounds)), floorf(CGRectGetMidY(superviewBounds)));
-    
-    [UIView animateWithDuration:duration
-                          delay:0
-                        options:UIViewAnimationOptionAllowUserInteraction
-                     animations:^{
-                         if (UIInterfaceOrientationIsPortrait(self.interfaceOrientation) && UIInterfaceOrientationIsLandscape(toInterfaceOrientation)) {
-                             self.containerView.transform = CGAffineTransformIdentity;
-                             self.containerView.center = superviewCenter;
-                         }
-                         if (UIInterfaceOrientationIsLandscape(self.interfaceOrientation) && UIInterfaceOrientationIsPortrait(toInterfaceOrientation)) {
-                             CGFloat portraitOffset = (superviewWidth - superviewHeight)/2;
-                             self.containerView.transform = CGAffineTransformMakeRotation(-M_PI/2);
-                             self.containerView.center = CGPointMake(superviewCenter.x-portraitOffset, superviewCenter.y+portraitOffset);
-                         }
-                         self.containerView.bounds = CGRectIntegral(superviewBounds);
-                         if (self.frameStyle != SCHStoryInteractionTitleOverlaysContents) {
-                             self.backgroundView.center = superviewCenter;
-                         }
-                     }
-                     completion:nil];
-}
-
 - (void)setupGeometryForContainerView:(UIView *)container
                        backgroundView:(UIImageView *)background
                          contentsView:(UIView *)contents
@@ -434,6 +402,38 @@
 }
 
 #pragma mark - orientation
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    if (!self.containerView.superview) {
+        return;
+    }
+    
+    CGFloat superviewWidth = CGRectGetWidth(self.containerView.superview.bounds);
+    CGFloat superviewHeight = CGRectGetHeight(self.containerView.superview.bounds);
+    CGRect superviewBounds = CGRectMake(0, 0, MAX(superviewWidth, superviewHeight), MIN(superviewWidth, superviewHeight));
+    CGPoint superviewCenter = CGPointMake(floorf(CGRectGetMidX(superviewBounds)), floorf(CGRectGetMidY(superviewBounds)));
+    
+    [UIView animateWithDuration:duration
+                          delay:0
+                        options:UIViewAnimationOptionAllowUserInteraction
+                     animations:^{
+                         if (UIInterfaceOrientationIsPortrait(self.interfaceOrientation) && UIInterfaceOrientationIsLandscape(toInterfaceOrientation)) {
+                             self.containerView.transform = CGAffineTransformIdentity;
+                             self.containerView.center = superviewCenter;
+                         }
+                         if (UIInterfaceOrientationIsLandscape(self.interfaceOrientation) && UIInterfaceOrientationIsPortrait(toInterfaceOrientation)) {
+                             CGFloat portraitOffset = (superviewWidth - superviewHeight)/2;
+                             self.containerView.transform = CGAffineTransformMakeRotation(-M_PI/2);
+                             self.containerView.center = CGPointMake(superviewCenter.x-portraitOffset, superviewCenter.y+portraitOffset);
+                         }
+                         self.containerView.bounds = CGRectIntegral(superviewBounds);
+                         if (self.frameStyle != SCHStoryInteractionTitleOverlaysContents) {
+                             self.backgroundView.center = superviewCenter;
+                         }
+                     }
+                     completion:nil];
+}
 
 - (void)didRotateToInterfaceOrientation:(UIInterfaceOrientation)aToInterfaceOrientation
 {
