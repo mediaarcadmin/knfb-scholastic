26d61949460f47c93f3a2a5cc4b56ffb815b5cbb
Neil Gall <neil.gall@bitwink.com>
2011-07-21 14:21:36 +0100

    [touch: 470] Move login sequence logic to shared ProfileViewController class so it can be reused for iPhone. Fix bug where keyboard does not hide. Move re-login logic out of SettingsViewController and pop back to ProfileViewController at first launch state after deregistration.

---
+++ Shared/SCHNavigationControllerForModalForm.h
@@ -0,0 +1,16 @@
+//
+//  SCHNavigationControllerForModalForm.h
+//  Scholastic
+//
+//  Created by Neil Gall on 21/07/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SCHNavigationControllerForModalForm : UINavigationController {
+    
+}
+
+@end
