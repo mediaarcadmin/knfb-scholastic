836ec5f34f63bf4525621fc5fdd4ff7bd71a10a1
Neil Gall <neil.gall@bitwink.com>
2011-07-26 14:04:10 +0100

    [touch: 498] Refactor book updates into separate model and controller

---
+++ Shared/SCHBookUpdates.h
@@ -0,0 +1,25 @@
+//
+//  SCHBookUpdates.h
+//  Scholastic
+//
+//  Created by Neil Gall on 26/07/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SCHBookUpdates : NSObject {}
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+// re-query the database for available book updates
+- (void)refresh;
+
+// YES if at least one book update is available
+- (BOOL)areBookUpdatesAvailable;
+
+// the set of NSManagedObjectIDs for the SCHAppBooks that can be updated
+- (id<NSFetchedResultsSectionInfo>)managedObjectIDsForAvailableBookUpdates;
+
+@end
