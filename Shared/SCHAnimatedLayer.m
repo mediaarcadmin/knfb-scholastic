42ea327a956f1b786fce01c539c6f4485f552112
Neil Gall <neil.gall@bitwink.com>
2011-08-18 10:52:28 +0100

    [touch: 559] Word Bird lose animations complete

--- Shared/SCHAnimatedLayer.m
+++ Shared/SCHAnimatedLayer.m
@@ -8,10 +8,13 @@
 
 #import "SCHAnimatedLayer.h"
 
+#define kPlayAnimationKey @"playAnimation"
+
 @implementation SCHAnimatedLayer
 
 @synthesize frameSize;
 @synthesize frameIndex;
+@synthesize numberOfFrames;
 
 + (BOOL)needsDisplayForKey:(NSString *)key
 {
@@ -31,10 +34,14 @@
 
 - (NSInteger)numberOfFrames
 {
-    CGImageRef image = (CGImageRef)self.contents;
-    NSInteger cols = CGImageGetWidth(image)/self.frameSize.width;
-    NSInteger rows = CGImageGetHeight(image)/self.frameSize.height;
-    return rows*cols;
+    if (numberOfFrames == 0) {
+        // default to the full number of frames in the image
+        CGImageRef image = (CGImageRef)self.contents;
+        NSInteger cols = CGImageGetWidth(image)/self.frameSize.width;
+        NSInteger rows = CGImageGetHeight(image)/self.frameSize.height;
+        numberOfFrames = rows*cols;
+    }
+    return numberOfFrames;
 }
 
 - (void)display
@@ -42,28 +49,28 @@
     size_t width = CGImageGetWidth((CGImageRef)self.contents);
     size_t height = CGImageGetHeight((CGImageRef)self.contents);
     CGSize size = [(SCHAnimatedLayer *)self.modelLayer frameSize];
+    if (width == 0 || height == 0 || size.width == 0) {
+        return;
+    }
+    
     NSInteger framesPerRow = width/size.width;
-    NSInteger index = [(SCHAnimatedLayer *)self.presentationLayer frameIndex];
+    NSInteger index = [self animationForKey:kPlayAnimationKey] ? [(SCHAnimatedLayer *)self.presentationLayer frameIndex] : self.frameIndex;
     NSInteger row = index/framesPerRow;
     NSInteger col = index%framesPerRow;
     self.contentsRect = CGRectMake((size.width*col)/width, (size.height*row)/height, size.width/width, size.height/height);
-    
-    NSLog(@"frame index %d  (%d)", index, self.frameIndex);
 }
 
 - (void)animateAllFramesWithDuration:(CFTimeInterval)duration
                             delegate:(id)delegate
 {
-    NSInteger lastFrame = self.numberOfFrames-1;
     CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"frameIndex"];
     anim.fromValue = [NSNumber numberWithInteger:0];
-    anim.toValue = [NSNumber numberWithInteger:lastFrame];
+    anim.toValue = [NSNumber numberWithInteger:self.numberOfFrames-1];
     anim.duration = duration;
+    anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    anim.fillMode = kCAFillModeForwards;
     anim.delegate = delegate;
-    [self addAnimation:anim forKey:@"animation"];
-    
-    self.frameIndex = lastFrame;
-    [self setNeedsDisplay];
+    [self addAnimation:anim forKey:kPlayAnimationKey];
 }
 
 @end
