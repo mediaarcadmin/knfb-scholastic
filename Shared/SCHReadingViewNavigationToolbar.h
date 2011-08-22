b6946b3084e9264de6363d3fbecd05f2f5fbb244
Matt Farrugia <matt.farrugia@bitwink.com>
2011-08-22 11:47:17 +0100

    Placeholder SCHNavigationToolbar

---
+++ Shared/SCHReadingViewNavigationToolbar.h
@@ -0,0 +1,16 @@
+//
+//  SCHReadingViewNavigationToolbar.h
+//  Scholastic
+//
+//  Created by Matt Farrugia on 22/08/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SCHReadingViewNavigationToolbar : UIView {
+    
+}
+
+@end
---
+++ Shared/SCHReadingViewNavigationToolbar.m
@@ -0,0 +1,37 @@
+//
+//  SCHReadingViewNavigationToolbar.m
+//  Scholastic
+//
+//  Created by Matt Farrugia on 22/08/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import "SCHReadingViewNavigationToolbar.h"
+
+
+@implementation SCHReadingViewNavigationToolbar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
