77063acb2112ee1772b088bcd515121fa716e068
Neil Gall <neil.gall@bitwink.com>
2011-08-24 10:44:24 +0100

    [touch: 561] Add opening screens for New Ending and Favorite variations of Picture Starter.

---
+++ Shared/SCHStoryInteractionControllerPictureStarterFavorite.h
@@ -0,0 +1,15 @@
+//
+//  SCHStoryInteractionControllerPictureStarterFavorite.h
+//  Scholastic
+//
+//  Created by Neil Gall on 24/08/2011.
+//  Copyright 2011 BitWink. All rights reserved.
+//
+
+#import "SCHStoryInteractionControllerPictureStarter.h"
+
+@interface SCHStoryInteractionControllerPictureStarterFavorite : SCHStoryInteractionControllerPictureStarter
+
+- (IBAction)goTapped:(id)sender;
+
+@end
