4f469952dfdecd320c43e59d4710c5065a4bf239
Neil Gall <neil.gall@bitwink.com>
2011-09-09 11:36:06 +0100

    [touch: 722] Add comment in SCHNavigationControllerForModalForm that this only works on iOS 4.3+

--- Shared/SCHNavigationControllerForModalForm.h
+++ Shared/SCHNavigationControllerForModalForm.h
@@ -11,5 +11,8 @@
 // Allows the use of a NavigationController in a modal form, dismissing the
 // keyboard when pushing new ViewControllers into the navigation controller.
 
+// Note: this is polish for iOS4.3+ only. Older OS versions require the user to
+// press the keyboard dismiss button.
+
 @interface SCHNavigationControllerForModalForm : UINavigationController {}
 @end
