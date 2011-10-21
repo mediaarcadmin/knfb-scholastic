8cd83f6bfb75f2981c228300b581debb684b5af9
Matt Farrugia <matt.farrugia@bitwink.com>
2011-10-21 09:31:33 +0100

    Remove exit button from setup bookshelves

--- Shared/SCHSetupBookshelvesViewController.m
+++ Shared/SCHSetupBookshelvesViewController.m
@@ -43,7 +43,7 @@
     [super viewDidLoad];
     [self setButtonBackground:self.setupBookshelvesButton];
     
-#if 1 // force a way out of this screen
+#if 0 // force a way out of this screen
     [self.setupBookshelvesButton setTitle:@"EXIT" forState:UIControlStateNormal];
     [self.setupBookshelvesButton addTarget:self.profileSetupDelegate action:@selector(showCurrentSamplesAnimated:) forControlEvents:UIControlEventTouchDown];
 #endif
