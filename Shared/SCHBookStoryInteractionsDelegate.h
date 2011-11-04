cfea3e6b316470f86144ea323045e52d4c5f155c
Neil Gall <neil.gall@bitwink.com>
2011-11-04 13:05:55 +0000

    Pass page size through to SCHBookStoryInteractions and on to each StoryInteraction on querying the questions associated with each page, so that Touch The Page can evaluate this correctly.

---
+++ Shared/SCHBookStoryInteractionsDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SCHBookStoryInteractionsDelegate.h
+//  Scholastic
+//
+//  Created by Neil Gall on 03/11/2011.
+//  Copyright (c) 2011 BitWink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SCHBookStoryInteractionsDelegate <NSObject>
+
+@end
