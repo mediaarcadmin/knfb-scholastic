a9a3a9361f604e4d25843670306dd354fba676e9
Matt Farrugia <matt.farrugia@bitwink.com>
2011-11-09 10:29:36 +0000

    Alert user that device is registered to another device if we recieve error code 50 or 144 [completed: 1046]

--- Shared/SCHDrmSession.h
+++ Shared/SCHDrmSession.h
@@ -16,6 +16,8 @@ static NSInteger const kSCHDrmRegistrationError = 2002;
 static NSInteger const kSCHDrmDeregistrationError = 2003;
 static NSInteger const kSCHDrmLicenseAcquisitionError = 2004;
 static NSInteger const kSCHDrmDeviceLimitError = 2005;
+static NSInteger const kSCHDrmDeviceRegisteredToAnotherDevice = 50;
+static NSInteger const kSCHDrmDeviceUnableToAssign = 144;
 
 #if UATSERVER
 static NSString* const drmServerUrl = @"http://plr.uat.cld.libredigital.com/rightsmanager.asmx";
