157c6572895b83967f85d930d31707fe34bfb90b
John S. Eddie <john.eddie@bitwink.com>
2012-03-02 16:25:02 +0000

    Added initial schema for wishlists

---
+++ Shared/SCHWishListItem.h
@@ -0,0 +1,25 @@
+//
+//  SCHWishListItem.h
+//  Scholastic
+//
+//  Created by John Eddie on 02/03/2012.
+//  Copyright (c) 2012 BitWink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+extern NSString * const kSCHWishListItem;
+
+@class SCHWishListProfile;
+
+@interface SCHWishListItem : NSManagedObject
+
+@property (nonatomic, retain) NSString * author;
+@property (nonatomic, retain) NSString * initiatedBy;
+@property (nonatomic, retain) NSString * isbn;
+@property (nonatomic, retain) NSDate * timestamp;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) SCHWishListProfile *wishListProfile;
+
+@end
---
+++ Shared/SCHWishListProfile.h
@@ -0,0 +1,29 @@
+//
+//  SCHWishListProfile.h
+//  Scholastic
+//
+//  Created by John Eddie on 02/03/2012.
+//  Copyright (c) 2012 BitWink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+extern NSString * const kSCHWishListProfile;
+
+@interface SCHWishListProfile : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * profileID;
+@property (nonatomic, retain) NSString * profileName;
+@property (nonatomic, retain) NSDate * timestamp;
+@property (nonatomic, retain) NSSet *itemList;
+@end
+
+@interface SCHWishListProfile (CoreDataGeneratedAccessors)
+
+- (void)addItemListObject:(NSManagedObject *)value;
+- (void)removeItemListObject:(NSManagedObject *)value;
+- (void)addItemList:(NSSet *)values;
+- (void)removeItemList:(NSSet *)values;
+
+@end
