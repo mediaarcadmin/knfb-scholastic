77c7030acfd50ce68b098ea68a39243886aabc09
John S. Eddie <john.eddie@bitwink.com>
2012-03-13 14:00:40 +0000

    disable web service logging

---
+++ Shared/SCHRecommendationWebService.m
@@ -0,0 +1,13 @@
+//
+//  SCHRecommendationWebService.m
+//  Scholastic
+//
+//  Created by John Eddie on 13/03/2012.
+//  Copyright (c) 2012 BitWink. All rights reserved.
+//
+
+#import "SCHRecommendationWebService.h"
+
+@implementation SCHRecommendationWebService
+
+@end
