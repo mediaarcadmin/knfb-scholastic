cfc8bf4805b8ed64449f86745c78b875f4a2a84b
Matt Farrugia <matt.farrugia@bitwink.com>
2012-03-21 15:09:36 +0000

    Placeholder empty SCHRecommendationManager

---
+++ Shared/SCHRecommendationManager.h
@@ -0,0 +1,13 @@
+//
+//  SCHRecommendationManager.h
+//  Scholastic
+//
+//  Created by Matt Farrugia on 21/03/2012.
+//  Copyright (c) 2012 BitWink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCHRecommendationManager : NSObject
+
+@end
---
+++ Shared/SCHRecommendationManager.m
@@ -0,0 +1,13 @@
+//
+//  SCHRecommendationManager.m
+//  Scholastic
+//
+//  Created by Matt Farrugia on 21/03/2012.
+//  Copyright (c) 2012 BitWink. All rights reserved.
+//
+
+#import "SCHRecommendationManager.h"
+
+@implementation SCHRecommendationManager
+
+@end
