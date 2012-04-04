c49d446f9325faa9cb3ed9c79b14c0ad89c637ee
Gordon Christie <gordon.christie@bitwink.com>
2012-04-04 11:44:33 +0100

    Retina stars and menu button, adjustment to iPhone list cells - now shows title/subtitle on separate lines.

--- Shared/SCHBookShelfTableViewCell.m
+++ Shared/SCHBookShelfTableViewCell.m
@@ -15,7 +15,9 @@
 #import <CoreText/CoreText.h>
 #import "SCHThemeManager.h"
 
-static NSInteger const CELL_TEXT_LABEL_TAG = 100;
+//static NSInteger const CELL_TEXT_LABEL_TAG = 100;
+static NSInteger const CELL_TEXT_TITLE_LABEL_TAG = 110;
+static NSInteger const CELL_TEXT_SUBTITLE_LABEL_TAG = 111;
 static NSInteger const CELL_BOOK_COVER_VIEW_TAG = 101;
 static NSInteger const CELL_NEW_INDICATOR_TAG = 102;
 static NSInteger const CELL_SAMPLE_SI_INDICATOR_TAG = 103;
@@ -32,7 +34,9 @@ static NSInteger const CELL_STAR_PERSONAL_RATING_VIEW = 302;
 
 @interface SCHBookShelfTableViewCell ()
 
-@property (readonly) TTTAttributedLabel *textLabel;
+//@property (readonly) TTTAttributedLabel *textLabel;
+@property (readonly) UILabel *titleLabel;
+@property (readonly) UILabel *subtitleLabel;
 @property (readonly) SCHBookCoverView *bookCoverView;
 @property (readonly) UIImageView *sampleAndSIIndicatorIcon;
 @property (readonly) UIView *backgroundView;
@@ -69,8 +73,10 @@ static NSInteger const CELL_STAR_PERSONAL_RATING_VIEW = 302;
     self = [super initWithCoder:aDecoder];
     
     if (self) {
-        self.textLabel.backgroundColor = [UIColor clearColor];
-        self.textLabel.text = [[[NSAttributedString alloc] initWithString:@""] autorelease];
+//        self.textLabel.backgroundColor = [UIColor clearColor];
+//        self.textLabel.text = [[[NSAttributedString alloc] initWithString:@""] autorelease];
+        self.titleLabel.backgroundColor = [UIColor clearColor];
+        self.subtitleLabel.backgroundColor = [UIColor clearColor];
         [self updateTheme];
         self.ruleImageView.image = [[UIImage imageNamed:@"ListViewRule"] stretchableImageWithLeftCapWidth:0 topCapHeight:0];
         
@@ -162,46 +168,53 @@ static NSInteger const CELL_STAR_PERSONAL_RATING_VIEW = 302;
 
     [self setNeedsDisplay];
 
-    NSString *titleString = nil;
-    float fontSize = 16.0f;
+//    NSString *titleString = nil;
+//    float fontSize = 16.0f;
+//    
+//    NSString *bookTitle  = book.Title;
+//    NSString *bookAuthor = book.Author;
+//    
+//    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+//        titleString = [NSString stringWithFormat:@"%@\n%@", bookTitle, bookAuthor];
+//    } else {
+////        titleString = [NSString stringWithFormat:@"%@ - %@", book.Title, book.Author];
+//        titleString = [NSString stringWithFormat:@"%@\n%@", book.Title, book.Author];
+//        fontSize = 10.0f;
+//    }
+//    NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:titleString];
+//
+//    UIFont *boldLabelFont = [UIFont fontWithName:@"Arial-BoldMT" size:fontSize];
+//    UIFont *labelFont = [UIFont fontWithName:@"Arial" size:fontSize];
+//    
+//    CTFontRef boldArialFont = CTFontCreateWithName((CFStringRef)boldLabelFont.fontName, boldLabelFont.pointSize, NULL);
+//    CTFontRef arialFont = CTFontCreateWithName((CFStringRef)labelFont.fontName, labelFont.pointSize, NULL);
+//
+//    if (arialFont && boldArialFont) {
+//        [attrString addAttribute:(NSString *)kCTFontAttributeName value:(id)boldArialFont range:NSMakeRange(0, [book.Title length])];
+////        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+//            [attrString addAttribute:(NSString *)kCTFontAttributeName value:(id)arialFont range:NSMakeRange([book.Title length], [book.Author length] + 1)];
+////        } else {
+////            [attrString addAttribute:(NSString *)kCTFontAttributeName value:(id)arialFont range:NSMakeRange([book.Title length], [book.Author length] + 3)];
+////        }
+//        
+//        CFRelease(arialFont);
+//        CFRelease(boldArialFont);
+//        
+//        [attrString addAttribute:(NSString *)kCTForegroundColorAttributeName value:(id)[UIColor SCHDarkBlue1Color].CGColor range:NSMakeRange(0, [titleString length])];
+//        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+//            [attrString addAttribute:(NSString *)kCTKernAttributeName value:(id)[NSNumber numberWithFloat:-0.4] range:NSMakeRange(0, [titleString length])];
+//        } else {
+//            [attrString addAttribute:(NSString *)kCTKernAttributeName value:(id)[NSNumber numberWithFloat:-0.3] range:NSMakeRange(0, [titleString length])];
+//        }
+//    }
+//    
+//    [self.textLabel setText:attrString];
+//    
+//    self.textLabel.backgroundColor = [UIColor clearColor];
+//    [attrString release];
     
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        titleString = [NSString stringWithFormat:@"%@\n%@", book.Title, book.Author];
-    } else {
-        titleString = [NSString stringWithFormat:@"%@ - %@", book.Title, book.Author];
-        fontSize = 10.0f;
-    }
-    NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:titleString];
-
-    UIFont *boldLabelFont = [UIFont fontWithName:@"Arial-BoldMT" size:fontSize];
-    UIFont *labelFont = [UIFont fontWithName:@"Arial" size:fontSize];
-    
-    CTFontRef boldArialFont = CTFontCreateWithName((CFStringRef)boldLabelFont.fontName, boldLabelFont.pointSize, NULL);
-    CTFontRef arialFont = CTFontCreateWithName((CFStringRef)labelFont.fontName, labelFont.pointSize, NULL);
-
-    if (arialFont && boldArialFont) {
-        [attrString addAttribute:(NSString *)kCTFontAttributeName value:(id)boldArialFont range:NSMakeRange(0, [book.Title length])];
-        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-            [attrString addAttribute:(NSString *)kCTFontAttributeName value:(id)arialFont range:NSMakeRange([book.Title length], [book.Author length] + 1)];
-        } else {
-            [attrString addAttribute:(NSString *)kCTFontAttributeName value:(id)arialFont range:NSMakeRange([book.Title length], [book.Author length] + 3)];
-        }
-        
-        CFRelease(arialFont);
-        CFRelease(boldArialFont);
-        
-        [attrString addAttribute:(NSString *)kCTForegroundColorAttributeName value:(id)[UIColor SCHDarkBlue1Color].CGColor range:NSMakeRange(0, [titleString length])];
-        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-            [attrString addAttribute:(NSString *)kCTKernAttributeName value:(id)[NSNumber numberWithFloat:-0.4] range:NSMakeRange(0, [titleString length])];
-        } else {
-            [attrString addAttribute:(NSString *)kCTKernAttributeName value:(id)[NSNumber numberWithFloat:-0.3] range:NSMakeRange(0, [titleString length])];
-        }
-    }
-    
-    [self.textLabel setText:attrString];
-    
-    self.textLabel.backgroundColor = [UIColor clearColor];
-    [attrString release];
+    self.titleLabel.text = book.Title;
+    self.subtitleLabel.text = book.Author;
     
     SCHAppBookFeatures bookFeatures = book.bookFeatures;
     
@@ -363,9 +376,19 @@ static NSInteger const CELL_STAR_PERSONAL_RATING_VIEW = 302;
 
 #pragma mark - Convenience Methods for Tagged Views
 
-- (TTTAttributedLabel *)textLabel
+//- (TTTAttributedLabel *)textLabel
+//{
+//    return (TTTAttributedLabel *)[self.contentView viewWithTag:CELL_TEXT_LABEL_TAG];
+//}
+
+- (UILabel *)titleLabel
 {
-    return (TTTAttributedLabel *)[self.contentView viewWithTag:CELL_TEXT_LABEL_TAG];
+    return (UILabel *)[self.contentView viewWithTag:CELL_TEXT_TITLE_LABEL_TAG];
+}
+
+- (UILabel *)subtitleLabel
+{
+    return (UILabel *)[self.contentView viewWithTag:CELL_TEXT_SUBTITLE_LABEL_TAG];
 }
 
 - (SCHBookCoverView *)bookCoverView
