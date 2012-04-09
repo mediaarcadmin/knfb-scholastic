60d187f4c4965c3948a89b29c6a357943cb7cff1
Matt Farrugia <matt.farrugia@bitwink.com>
2012-04-09 11:06:15 +0100

    Remove runtime attributes so that iOS4 works

--- Shared/SCHRecommendationContainerView.m
+++ Shared/SCHRecommendationContainerView.m
@@ -12,11 +12,23 @@
 
 @synthesize container;
 @synthesize box;
+@synthesize title;
+
+- (void)awakeFromNib
+{
+    self.title.layer.cornerRadius = 10;
+    self.title.layer.borderWidth = 1;
+    self.title.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    
+    self.box.layer.borderWidth = 1;
+    self.box.layer.borderColor = [UIColor lightGrayColor].CGColor;
+}
 
 - (void)dealloc
 {
     [container release], container = nil;
     [box release], box = nil;
+    [title release], title = nil;
     [super dealloc];
 }
 
