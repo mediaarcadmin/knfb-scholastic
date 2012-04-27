cca042893dea4d556c16708d250e2997c101967c
Neil Gall <neil.gall@bitwink.com>
2012-04-27 10:37:22 +0100

    [completed: 1463] Fix layout and zooming behaviour on iPhone Graphic SI

--- Shared/SCHStoryInteractionControllerImage.m
+++ Shared/SCHStoryInteractionControllerImage.m
@@ -33,41 +33,22 @@
     self.imageView.image = [UIImage imageWithData:imageData];
     
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        self.imageView.frame = self.scrollView.bounds;
         [self.scrollView addSubview:self.imageView];
         self.scrollView.delegate = self;
-        
-        CGSize imageSize = self.imageView.image.size;
-        if (imageSize.width > 0.0 && imageSize.height > 0.0) {
-            CGRect rect = self.imageView.bounds;
-            rect.size = imageSize;
-            self.imageView.bounds = rect;        
-            self.imageView.frame = rect;
-            self.scrollView.contentSize = imageSize;
-            self.scrollView.layer.cornerRadius = 10.0;  
-            self.scrollView.layer.borderWidth = 0.0;
-            self.scrollView.layer.masksToBounds = YES;
-            
-            CGFloat widthScale = CGRectGetWidth(self.scrollView.bounds) / imageSize.width;
-            CGFloat heightScale = CGRectGetHeight(self.scrollView.bounds) / imageSize.height;
-            self.scrollView.maximumZoomScale = MAX(widthScale, heightScale) * 1.5;    // restrict the zoom so we don't completely pixelize the image 
-            self.scrollView.minimumZoomScale = MIN(widthScale, heightScale);
-            
-            self.scrollView.zoomScale = widthScale;
-        }
+        self.scrollView.contentSize = self.scrollView.bounds.size;
+        self.scrollView.layer.cornerRadius = 10.0;  
+        self.scrollView.layer.borderWidth = 0.0;
+        self.scrollView.layer.masksToBounds = YES;
+        self.scrollView.maximumZoomScale = 1.5;    // restrict the zoom so we don't completely pixelize the image 
+        self.scrollView.minimumZoomScale = 1.0;
+        self.scrollView.zoomScale = 1.0;
     } else {
         [self resizeCurrentViewToSize:self.imageView.image.size animationDuration:0 withAdditionalAdjustments:nil];
-        self.scrollView.hidden = YES;
     } 
     
     // Always mark the SI as complete as soon as it is opened once
     self.controllerState = SCHStoryInteractionControllerStateInteractionFinishedSuccessfully;
-    
-    self.imageView.layer.borderColor = [UIColor purpleColor].CGColor;
-    self.imageView.layer.borderWidth = 1;
-    
-    self.scrollView.layer.borderColor = [UIColor orangeColor].CGColor;
-    self.scrollView.layer.borderWidth = 1;
-
 }
 
 - (SCHFrameStyle)frameStyle
@@ -75,23 +56,22 @@
     return(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ? SCHStoryInteractionFullScreen : SCHStoryInteractionNoTitle);
 }
 
-- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
-{
-    [super willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
-}
-
 #pragma mark - UIScrollView delegate methods
 
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
 {
-    return(self.imageView);
+    return self.imageView;
 }
 
-- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView 
-                       withView:(UIView *)view 
-                        atScale:(float)scale
+- (void)scrollViewDidZoom:(UIScrollView *)aScrollView
 {
-    // nop - method included to implement zooming
+    CGFloat scale = aScrollView.zoomScale;
+    if (scale < 1.0) {
+        CGSize scaledSize = CGSizeMake(aScrollView.contentSize.width*scale, aScrollView.contentSize.height*scale);
+        CGFloat x = (aScrollView.contentSize.width-scaledSize.width)/2;
+        CGFloat y = (aScrollView.contentSize.height-scaledSize.height)/2;
+        aScrollView.contentOffset = CGPointMake(-x/scale, -y/scale);
+    }
 }
 
 #pragma mark - Override for SCHStoryInteractionControllerStateReactions
