466bbe8090441b813ae9ff40c12cde790ac8bd57
John S. Eddie <john.eddie@bitwink.com>
2012-06-12 10:13:48 +0100

    [completed: 1609] we now use Version and not LastVersion in all areas of the app

--- Shared/SCHReadingViewController.m
+++ Shared/SCHReadingViewController.m
@@ -2107,7 +2107,7 @@ static const NSUInteger kReadingViewMaxRecommendationsCount = 4;
 
         SCHAppContentProfileItem *appContentProfileItem = [profile appContentProfileItemForBookIdentifier:self.bookIdentifier];
         if (appContentProfileItem != nil) {
-            newHighlight.Version = [NSNumber numberWithInteger:[appContentProfileItem.ContentProfileItem.UserContentItem.LastVersion integerValue]];
+            newHighlight.Version = [NSNumber numberWithInteger:[appContentProfileItem.ContentProfileItem.UserContentItem.Version integerValue]];
         }
         [self save];
     }
@@ -2812,7 +2812,7 @@ static const NSUInteger kReadingViewMaxRecommendationsCount = 4;
         
         SCHAppContentProfileItem *appContentProfileItem = [profile appContentProfileItemForBookIdentifier:self.bookIdentifier];
         if (appContentProfileItem != nil) {
-            scratchNote.Version = [NSNumber numberWithInteger:[appContentProfileItem.ContentProfileItem.UserContentItem.LastVersion integerValue]];
+            scratchNote.Version = [NSNumber numberWithInteger:[appContentProfileItem.ContentProfileItem.UserContentItem.Version integerValue]];
         }
         
         SCHBookPoint *currentPoint = [self.readingView currentBookPoint];
