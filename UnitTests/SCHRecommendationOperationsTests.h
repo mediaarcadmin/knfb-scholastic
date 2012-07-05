3dfc98da8eaca871622f01e483105f512624424f
Gordon Christie <gordon.christie@bitwink.com>
2012-07-05 12:32:53 +0100

    First version of Book Data tests - tests SCHBookPoint. Also work in progress on Recommendation operation tests.

---
+++ UnitTests/SCHRecommendationOperationsTests.h
@@ -0,0 +1,13 @@
+//
+//  SCHRecommendationOperationsTests.h
+//  Scholastic
+//
+//  Created by Gordon Christie on 05/07/2012.
+//  Copyright (c) 2012 BitWink. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SCHRecommendationOperationsTests : SenTestCase
+
+@end
