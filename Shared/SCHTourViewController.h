4edbba9f823dd62ea9b808af6d0e8ad1da9fed18
Gordon Christie <gordon.christie@bitwink.com>
2012-09-17 10:02:24 +0100

    Added a placeholder for the Tour View Controller.

---
+++ Shared/SCHTourViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCHTourViewController.h
+//  Scholastic
+//
+//  Created by Gordon Christie on 17/09/2012.
+//  Copyright (c) 2012 BitWink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCHTourViewController : UIViewController
+
+@end
