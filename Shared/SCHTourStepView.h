5e0b1ad82b5e86ae68a39fc312db935fb249dc24
Gordon Christie <me@gordonchristie.com>
2012-09-27 14:03:26 +0100

    File tidying.

---
+++ Shared/SCHTourStepView.h
@@ -0,0 +1,13 @@
+//
+//  SCHTourStepView.h
+//  Scholastic
+//
+//  Created by Gordon Christie on 27/09/2012.
+//  Copyright (c) 2012 BitWink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCHTourStepView : UIView
+
+@end
