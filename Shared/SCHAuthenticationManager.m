acecbdbd5bcc3253a68e92134682c8641d261d40
Matt Farrugia <matt.farrugia@bitwink.com>
2012-11-16 10:39:47 +0000

    Update 40 device limit message [completed: 2110]

--- Shared/SCHAuthenticationManager.m
+++ Shared/SCHAuthenticationManager.m
@@ -1117,7 +1117,7 @@ deregistrationDidFailWithError:(NSError *)error
                ([error code] == kSCHAccountValidationCredentialsMissingError)) {
         localizedMessage = NSLocalizedString(@"Your e-mail address or password was not recognized. Please try again.", nil);
     } else if ([error code] == kSCHDrmDeviceLimitError) {
-        localizedMessage = NSLocalizedString(@"Storia is already installed on forty devices, which is the maximum allowed. Before installing it on this device, you need to deregister Storia on one of your current devices.", nil);
+        localizedMessage = NSLocalizedString(@"Storia is already installed on 40 devices, which is the maximum allowed. Before installing Storia on this device, you to need to deregister it on one of your current devices. To do this, go to the Reading Manager on the device which you want to deregister.", nil);
     } else if (([error code] == kSCHDrmDeviceRegisteredToAnotherDevice) ||
                ([error code] == kSCHDrmDeviceUnableToAssign)) {
         localizedMessage = NSLocalizedString(@"This device is registered to another Scholastic account. The owner of that account needs to deregister this device before it can be registered to a new account.", nil);
